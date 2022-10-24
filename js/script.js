600c676c1622a2cc39932bfe71a36716dc4fb7fc
MarBozzz <mbozzuto@libero.it>
2022-10-24 17:10:07 +0200

    [added] timing function for autoplayer carousel

--- js/script.js
+++ js/script.js
@@ -1,68 +1,65 @@
-//creo l'array con le img
-const imagesArray = ['01.jpg', '02.jpg', '03.jpg', '04.jpg', '05.jpg'];
+const slider = document.querySelector('.slider');
+const thumbs = document.querySelector('.thumbs');
+const next = document.querySelector('.next');
+const prev = document.querySelector('.prev');
 
-//creo elemento dinamico tag img
-let imagesTags = '';
+const numImages = 5;
+let counterImages = 0;
+let sliderHtml = '';
+let thumbsHtml = '';
 
-//creo shortcut per queryselector
-const el = document.querySelector.bind(document);
-
-//creo la const  che racchiude il div contenitore delle img
-const slider =  el ('.slider');
-
-//const slider = document.querySelector ('.slider');
-
-for (let index = 0; index < imagesArray.length; index++) {
-  imagesTags += `<img class="item" src="img/${imagesArray[index]}" alt="${imagesArray[index]}">` ;
+for(let i = 1; i <= numImages; i++){
+  sliderHtml += `
+      <img  class="item" src="img/0${i}.jpg" alt="">
+  `;
+  thumbsHtml += `
+      <img  class="item-thumb" src="img/0${i}.jpg" alt="">
+  `;
 }
 
-//di default si deve vedere la prima foto, con indice 0
-let counterimages = 0;
+slider.innerHTML = sliderHtml;
+thumbs.innerHTML = thumbsHtml;
 
-const next = el ('.down');
-const previous = el ('.up');
+const listImages = document.getElementsByClassName('item');
+const listthumbs = document.getElementsByClassName('item-thumb');
 
-//di default non ci deve essere la scelta previous
-previous.classList.add('hide');
+listImages[counterImages].classList.add('active');
+listthumbs[counterImages].classList.add('active');
 
-// al div contenitore apporto gli img tag
-slider.innerHTML = imagesTags;
+prev.addEventListener('click',function(){
+  nextPrev(false);
+})
 
-//creo la collection di elementi HTML che hanno classe item
-const items = document.getElementsByClassName('item');
+next.addEventListener('click',function(){
+  nextPrev(true);
+})
 
-//aggiungo al primo elemento della collection la classe active 
-items[0].classList.add('active');
+function nextPrev(isNext){
+  listImages[counterImages].classList.remove('active');
+  listthumbs[counterImages].classList.remove('active');
+  if(isNext){
+    counterImages--;
+    if(counterImages < 0) counterImages = numImages - 1;
+  } else {
+    counterImages++;
+    
+    if(counterImages === numImages) counterImages = 0;
+  }
+  listImages[counterImages].classList.add('active');
+  listthumbs[counterImages].classList.add('active');
+}
 
-//al click, spostandosi alla foto successiva, tramite il contatore tolgo la classe active dalla foto visitata e la aggiungo alla successiva
-next.addEventListener('click', function(){
-  items[counterimages].classList.remove('active');
-  items[++counterimages].classList.add('active');
-//dalla seconda foto in poi si ripristina la scelta previous
-  previous.classList.remove('hide');
-//quando il numero del contatore è uguale a quello dell'array va nascosto il tasto next
-  if(counterimages === imagesArray.length) counterimages = 0
-    /*next.classList.add('hide');*/
-    //const imagesArrayBis = [...imagesArray];
-    //console.log(imagesArrayBis);
-    //imagesArray.push(imagesArrayBis);
-    //counterimages.reset[0];
-    //items[0].classList.add('active');
-    /*items[counterimages].classList.remove('active');
-    items[++counterimages].classList.add('active');*/
-    //items[4].classList.add('hide');
-    //items = document.getElementsByClassName('item');
-  
-;
+//aggiunte variabili per l'autoplay del carousel e per l'area del mouseover
+let selectedPic;
+const container = document.querySelector('.container');
 
+//aggiunta timing function per l'autoplay del carousel
+selectedPic = setInterval(nextPrev,2500);
 
-//allo stasso modo agisco sul bottone previous
-previous.addEventListener('click', function(){
-  items[counterimages].classList.remove('active');
-  items[--counterimages].classList.add('active');
-
-  next.classList.remove('hide');
-  /*if(counterimages === 0) {
-    previous.classList.add('hide');
-  }*/
+//aggiunto mouseover per interrompere l'autoplay del carousel e mouseout per farlo riprendere
+container.addEventListener('mouseover', function(){
+  clearInterval(selectedPic);
+});
+container.addEventListener('mouseout', function(){
+  selectedPic = setInterval(nextPrev,2500);
 });
